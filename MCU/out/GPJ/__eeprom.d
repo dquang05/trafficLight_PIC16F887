327c41b6f2ab0cf87a0c7b05f4490518d161c12f
TDQ <quang.trandanh05@hcmut.edu.vn>
2026-04-19 19:58:59 +0700

    Stop tracking editor/build artifacts; update .gitignore

--- MCU/out/GPJ/__eeprom.d
+++
@@ -1,2 +0,0 @@
-D:\Hardware\MPLab\GPJ\out\GPJ\__eeprom.p1: \
-  c:/PROGRA~1/MICROC~1/xc8/v3.10/pic/sources/c99/pic/__eeprom.c
--- MCU/out/GPJ/doprnt.d
+++
@@ -1,2 +0,0 @@
-D:\Hardware\MPLab\GPJ\out\GPJ\doprnt.p1: \
-  c:/PROGRA~1/MICROC~1/xc8/v3.10/pic/sources/c99/common/doprnt.c
